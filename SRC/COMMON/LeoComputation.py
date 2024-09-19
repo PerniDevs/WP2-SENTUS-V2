9bf302e7d218dbaf2800b6c4cc0839ef5df138c8
home <pernigotti.developments@gmail.com>
2024-09-19 13:29:35 -0700

    deleted comentary

--- SRC/COMMON/LeoComputation.py
+++ SRC/COMMON/LeoComputation.py
@@ -82,13 +82,6 @@ def computeRcvrApo(Conf, Year, Doy, Sod, SatLabel, LeoQuatInfo):
     q1 = LeoQuatInfo[LeoQuatInfo[LeoQuatIdx["SOD"]] == Sod][LeoQuatIdx["q1"]].iloc[0]
     q2 = LeoQuatInfo[LeoQuatInfo[LeoQuatIdx["SOD"]] == Sod][LeoQuatIdx["q2"]].iloc[0]
     q3 = LeoQuatInfo[LeoQuatInfo[LeoQuatIdx["SOD"]] == Sod][LeoQuatIdx["q3"]].iloc[0]
-
-    #   Normalize quaternion components
-    norm = np.sqrt(q0**2 + q1**2 + q2**2 + q3**2)
-    q0 /= norm
-    q1 /= norm
-    q2 /= norm
-    q3 /= norm
     
     R = np.array([[1-2*(q2**2)-2*(q3**2), 2*(q1*q2-q0*q3),       2*(q0*q2+q1*q3)],
                   [2*(q1*q2+q0*q3),       1-2*(q1**2)-2*(q3**2), 2*(q2*q3-q0*q1)],
